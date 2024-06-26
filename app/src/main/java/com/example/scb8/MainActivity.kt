803bf6dc027d1afe49784250fd2bb66e7f4ca4eb
abdul <abdul.tanveer@gmail.com>
2024-06-26 12:32:47 +0530

    lifecycle of an activity

--- app/src/main/java/com/example/scb8/MainActivity.kt
+++ app/src/main/java/com/example/scb8/MainActivity.kt
@@ -15,14 +15,47 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     lateinit var constraintLayout: ConstraintLayout
-
+    var TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         constraintLayout = findViewById(R.id.xmlConstraintLayout)
+        Log.i(TAG,"activity is getting created --egg")
+        //allocate memory for your activity
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG,"activity is getting started[visible click] --egg hatched")
+        //fetch the data while the user is interacting with ur app
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w(TAG,"activity resumed- visible again --woken")
+        //restore the state of nfs app
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(TAG,"activity paused- invisible --sleeping")
+        //save the state of the nfs app
 
     }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"activity stopped- hibernate --death")
+        //release the resources
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"activity destroyed- purged --no remains of hen")
+
+    }
+
 
     fun clickHandler(view: View) {
             var snackbar:Snackbar = Snackbar.make(constraintLayout,"message deleted",Snackbar.LENGTH_LONG)
