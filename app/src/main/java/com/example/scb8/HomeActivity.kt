1eff4baac79397930fd298646665185297cea0b8
abdul <abdul.tanveer@gmail.com>
2024-06-27 09:40:30 +0530

    recyclerview part-3 implemented the methods

--- app/src/main/java/com/example/scb8/HomeActivity.kt
+++ app/src/main/java/com/example/scb8/HomeActivity.kt
@@ -2,21 +2,24 @@ package com.example.scb8
 
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity() {
     lateinit var homeTextView: TextView
+    lateinit var adapter: RowCardAdapter
+    lateinit var recyclerView: RecyclerView
 
     var data = arrayOf("india","hindi","karnataka","tamilnadu","software")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
         homeTextView = findViewById(R.id.tvHome)
+        adapter = RowCardAdapter(data)
+        recyclerView.adapter = adapter
 
-       var dataReceived =  intent.extras?.getString("mykey")
-        homeTextView.setText(dataReceived)
     }
 }
--- app/src/main/java/com/example/scb8/RowCardAdapter.kt
+++ app/src/main/java/com/example/scb8/RowCardAdapter.kt
@@ -1,6 +1,42 @@
 package com.example.scb8
 
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class RowCardAdapter: RecyclerView.Adapter<ReserveCardHolder> {
+class RowCardAdapter(var data: Array<String>) : RecyclerView.Adapter<ReserveCardHolder>() {
+    var TAG = RowCardAdapter::class.java.simpleName
+
+    //ajay -- buy row card from market n give it to babu
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReserveCardHolder {
+        //LayoutInflater = inflates the baloon-- row_card
+        Log.i(TAG,"ajay bought the card")
+        var cardBoughtAjay:View = LayoutInflater.from(parent.context).inflate(R.layout.row_card,parent,false)
+        return  ReserveCardHolder(cardBoughtAjay)
+    }
+
+    //beatriz --keeping count of no of items in the dataset n informs ajay
+    override fun getItemCount(): Int {
+        Log.i(TAG,"beatriz counted"+data.size)
+        return data.size
+    }
+
+    //cath - get the card from babu, write the data on the card and insert it in the recyclerview
+    override fun onBindViewHolder(cardBoughtAjay: ReserveCardHolder, position: Int) {
+        Log.i(TAG,"cath is writing-"+data.get(position))
+
+        cardBoughtAjay.rowTextView.setText(data.get(position))
+    }
+}
+
+//babu -- keep the card bought by ajay in the reserver card holder
+class ReserveCardHolder(cardBoughtAjay:View):RecyclerView.ViewHolder(cardBoughtAjay) {
+    var rowTextView:TextView = cardBoughtAjay.findViewById(R.id.tvRowCard)
+    init {
+        Log.i("ReserveCard","babu got the card from ajay")
+    }
+
 }
